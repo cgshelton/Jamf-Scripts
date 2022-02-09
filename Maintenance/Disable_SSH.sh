9c3357ca3e1c3613d97a2530bd365afadf6f6c92
cgshelton <95441694+cgshelton@users.noreply.github.com>
2022-02-09 09:42:09 -0500

    Update Disable_SSH.sh
    cleaned up the code a smidge

--- Maintenance/Disable_SSH.sh
+++ Maintenance/Disable_SSH.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-
-#Disable Screen Sharing if it is enabled
+#defining variables for cheking the status of the screen sharing service
 ScreenSharingStatus=$(/usr/bin/sudo /bin/launchctl list com.apple.screensharing)
 ExpectedScreenSharingResult=" Could not find service \"com.apple.screensharing\" in domain for system"
 
-
-#This check doesn't work currently. We still get the intended outcome, however.
-if [ "$ScreenSharingStatus" = "$ExpectedScreenSharingResult" ]
-then
-  ScreenSharingOutput="Screen Sharing is already disabled"
-else
-  ScreenSharingOutput="Turning off Screen Sharing"
-  /usr/bin/sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
-fi
-
-#Disable SSH if it is enabled
+#defining variables for cheking the status of the SSH service
 SSH_Status=$(/usr/bin/sudo /usr/sbin/systemsetup -getremotelogin)
 ExpectedSSHResult="Remote Login: Off"
-SSHOutput="SSH Is already disabled"
 
+
+
+#if statement to check if screen sharing is enabled
+#This check doesn't work currently. We still get the intended outcome, however.
+if [ "$ScreenSharingStatus" != "$ExpectedScreenSharingResult" ]
+then
+  #Set output variable to notify that Screen Sharing will be turned of, then run command to turn it off
+  ScreenSharingOutput="Turning off Screen Sharing"
+  /usr/bin/sudo /bin/launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+else
+  #Set output variable to notify that Screen Sharing is already disabled
+  ScreenSharingOutput="Screen Sharing is already disabled"
+fi
+
+#if statement to check if SSH is enabled
 if [ "$SSH_Status" != "$ExpectedSSHResult" ]
 then
+#Set output variable to notify that SSH will be turned of, then run command to turn it off
  SSHOutput="Turning off SSH"
  /usr/bin/sudo /usr/sbin/systemsetup -f -setremotelogin off
+else
+  #Set output variable to notify that SSH is already disabled
+  SSHOutput="SSH Is already disabled"
 fi
 
 #Report back on what was done
