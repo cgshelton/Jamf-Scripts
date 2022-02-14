eb03d145f19b2ec5e147460aa0dc32f59abafb0c
cgshelton <95441694+cgshelton@users.noreply.github.com>
2022-02-14 16:45:53 -0500

    Update Disable_SSH.sh
    final changes before final testing.

--- Maintenance/Disable_SSH.sh
+++ Maintenance/Disable_SSH.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 #defining variables for cheking the status of the screen sharing service
-ScreenSharingStatus=$(/usr/bin/sudo /bin/launchctl list com.apple.screensharing)
-ExpectedScreenSharingResult="Could not find service \"com.apple.screensharing\" in domain for system"
+#these aren't needed because of issue with storing result in variable, may revisit later
+#ScreenSharingStatus=$(/usr/bin/sudo /bin/launchctl list com.apple.screensharing)
+#ExpectedScreenSharingResult="Could not find service \"com.apple.screensharing\" in domain for system"
 
-#defining variables for cheking the status of the SSH service
-SSH_Status=$(/usr/bin/sudo /usr/sbin/systemsetup -getremotelogin)
-ExpectedSSHResult="Remote Login: Off"
-
-
-
-#if statement to check if screen sharing is enabled
-#This check doesn't work currently. We still get the intended outcome, however.
-if [[ "$(sudo launchctl list com.apple.screensharing)" != "" ]]
+#if statement to check if screen sharing is enabled, for some reason, running the command below DOES NOT properly
+#store the result in a variabe in the event that it doesn't find the file, so we are looking for a blank string instead
+if [[ "$(/usr/bin/sudo /bin/launchctl list com.apple.screensharing)" != "" ]]
 then
   #Set output variable to notify that Screen Sharing will be turned of, then run command to turn it off
   ScreenSharingOutput="Turning off Screen Sharing"
@@ -21,6 +16,10 @@ else
   #Set output variable to notify that Screen Sharing is already disabled
   ScreenSharingOutput="Screen Sharing is already disabled"
 fi
+
+#defining variables for cheking the status of the SSH service
+SSH_Status=$(/usr/bin/sudo /usr/sbin/systemsetup -getremotelogin)
+ExpectedSSHResult="Remote Login: Off"
 
 #if statement to check if SSH is enabled
 if [ "$SSH_Status" != "$ExpectedSSHResult" ]
@@ -34,6 +33,7 @@ else
 fi
 
 #Report back on what was done
+echo ""
 echo $ScreenSharingOutput
 echo $SSHOutput
 
