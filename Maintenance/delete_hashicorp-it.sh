f40c3f46f76f5e08fb8c03b935ec44e80eb90673
cgshelton <95441694+cgshelton@users.noreply.github.com>
2022-08-18 17:43:50 -0400

    Update delete_hashicorp-it.sh
    Added error checking & logging. Script also stops if conditions are not met to properly delete account.

--- Maintenance/delete_hashicorp-it.sh
+++ Maintenance/delete_hashicorp-it.sh
@@ -1,10 +1,71 @@
 #!/bin/bash
+logfile="/Library/HashiCorpIT/Logs/account_delete.log"
+
+#look for log file
+if [ ! -f "$logfile" ] ; then
+	#if file does not exist, create it
+	touch "$logfile"
+fi
+
+###Prep variables for Logging###
+#Grab today's Date
+today=$(date)
+#spacer, to make the log file look pretty
+spacer=" : "
+#status, to report what happened
+status=""
+#entry="$today$spacer$status"
+#echo "$entry" >> "$logfile"
 
 ###### Collect User Credentials to perform the action #####
 
 #Collect current username
 CurrentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-#echo $CurrentUser
+
+#build and post to logfile
+status="Currently logged on user is $CurrentUser"
+entry="$today$spacer$status"
+echo "$entry" >> "$logfile"
+
+######## Before prompting user for password, check to see if we can successfully remove the account #########
+
+###Validation Checks before we can delete the account###
+
+#check local directory for presence of hashicorp-it account
+#accountStatus=$( /usr/bin/dscl . -read /Users/hashicorp-it )
+if [[ $(dscl . list /Users | grep hashicorp-it) == "" ]]; then
+	accountPresent="No"
+	status="hashicorp-it account not present, no action needed"
+	entry="$today$spacer$status"
+	echo "$entry" >> "$logfile"
+	cat "$logfile"
+	exit 0
+else
+	accountStatus="Yes"
+	status="hashicorp-it account present, needs to be deleted"
+	entry="$today$spacer$status"
+	echo "$entry" >> "$logfile"
+fi
+
+#confirm currently logged-in user has a secure token to perform the action we need
+#currentUserStatus=$( sysadminctl -secureTokenStatus $CurrentUser )
+currentUserStatus=$( dscl . -read /Users/$CurrentUser AuthenticationAuthority | grep -o SecureToken )
+
+#check if $currentUserStatus is empty, if so, account has no SecureToken
+if [ -z $currentUserStatus ]; then
+	tokenPresent="No"
+	status="account does not have a SecureToken, manual intervention required"
+	entry="$today$spacer$status"
+	echo "$entry" >> "$logfile"
+	cat "$logfile"
+	exit 0
+else
+	tokenPresent="Yes"
+	status="account has a SecureToken, can proceed with account deletion(if needed)"
+	entry="$today$spacer$status"
+	echo "$entry" >> "$logfile"
+fi
+
 
 #Prompt user for their Password
 userPassword=$(/usr/bin/osascript<<END
@@ -14,29 +75,51 @@ set the answer to text returned of (display dialog "Please provide your password
 END
 )
 
-#If users closes prompt & does not enter a password, return an error to Jamf console
-if [ -z $userPassword ]
-then
-echo "ERROR: User did not enter a password"
+#If users closes prompt & does not enter a password, log an error
+if [ -z $userPassword ]; then
+status="User did not enter a password"
 exit 1
+else
+status="User provided a password"
 fi
 
-###Validation Checks before we can delete the account###
-
-#check local directory for presence of hashicorp-it account
-accountStatus=$( /usr/bin/dscl . -read /Users/hashicorp-it )
-
-#confirm currently logged-in user has a secure token to perform the action we need
-currentUserStatus=$( sysadminctl -secureTokenStatus $CurrentUser )
-
+#build and post to logfile
+entry="$today$spacer$status"
+echo "$entry" >> "$logfile"
 
 
 ###Carry out account deletion action###
 
 #Delete account using dscl
 /usr/bin/dscl -u $CurrentUser -P $userPassword . -delete /Users/hashicorp-it
+#build and post to logfile
+status="deleting hashicorp-it account..."
+entry="$today$spacer$status"
+echo "$entry" >> "$logfile"
 
 #Delete hashicorp-it home folder
-/bin/rm -f /Users/hashicorp-it
+/bin/rm -rf /Users/hashicorp-it/
+status="deleting hashicorp-it home folder..."
+#build and post to logfile
+entry="$today$spacer$status"
+echo "$entry" >> "$logfile"
 
-exit 0 #Exit as successful
+#post logfile to console & exit
+cat "$logfile"
+exit 0
+
+###Final validation###
+#status="Performing final validation check"
+#entry="$today$spacer$status"
+#echo "$entry" >> "$logfile"
+
+#if [ $tokenPresent == "No" ]; then
+#	#build and post to logfile
+#	status="Unable to delete account, exiting..."
+#	entry="$today$spacer$status"
+#	echo "$entry" >> "$logfile"
+#	#post logfile to jamf logs
+#	cat "$logfile"
+#	#stop processing commands
+#	exit 0
+#fi
