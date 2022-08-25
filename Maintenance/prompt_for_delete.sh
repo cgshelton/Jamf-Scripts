5afa8f5921181d2f3691074a6c7c0f02e16fb669
smwaldman <38405926+smwaldman@users.noreply.github.com>
2022-08-25 00:20:33 -0700

    Update prompt_for_delete.sh
    • add short header to script (author and date are always useful for future)
    • add icon
    • change prompt type
    • add heading to prompt because headings don't suck with this prompt type
    • took a stab at cutting down on words
    • add button to open url to documentation
    • switch to double brackets to avoid errors

--- Maintenance/prompt_for_delete.sh
+++ Maintenance/prompt_for_delete.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Author: 
+#Date: 
+#About: This script checks for the presence of the hashicorp-it account and prompts the user to remove it if found
+
 #check local directory for presence of hashicorp-it account
 if [[ $(/usr/bin/dscl . list /Users | grep hashicorp-it ) == "" ]]; then
 	echo "hashicorp-it account not present, no action needed"
@@ -9,41 +13,46 @@ fi
 #unhide account, hopefully
 /usr/bin/dscl . create /Users/hashicorp-it IsHidden 0
 
+#get logged in user for launching app under user
+LOGGED_IN_USER=$(stat -f%Su /dev/console)
+LOGGED_IN_UID=$(id -u "$LOGGED_IN_USER")
+
+#check for hashi logo
+if [[ -e /Library/HashiCorpIT/Logos/HashiCorp_Logomark_White_zoomed.png ]]; then
+  ICON="/Library/HashiCorpIT/Logos/HashiCorp_Logomark_White_zoomed.png"
+else
+  #if running script from jamf, jamf policy -event hashiBranding
+  ICON="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertNoteIcon.icns"
+fi
 
 #Define variables to fill Jamf Helper popup with
 JAMFHELPER='/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper'
-TITLE="ACTION REQUIRED - Hashicorp IT - ACTION REQUIRED"
-DESC="To increase the security of your device, we need your assistance
-removing an old account.
+TITLE="HashiCorp IT"
+HEADING="***ACTION REQUIRED***"
+DESC="HashiCorp IT needs your assistance to remove an old account for security purposes. 
 
-      Please follow these instructions:
-      1) Click OK below, system preferences will open
-      2) Click the lock in the lower left & provide your password
-      3) Click 'Hashicorp-it' account, click the minus button
-      4) Choose 'delete the home folder', and click 'Delete User'
+     To remove the account:
+     1) Click 'Continue' to open System Preferences
+     2) Click the lock in the lower left & provide your password
+     3) Click 'Hashicorp-it', click the minus button
+     4) Choose 'delete the home folder', then click 'Delete User'
 
+Your assistance is greatly appreciated! 
 
-If you have any questions or concerns, please contact us by emailing helpdesk@hashicorp.com.
-
-Your assistance & cooperation is greatly appreciated!
-
--Hashicorp IT
-"
+For help, please email helpdesk@hashicorp.com"
 
 #Display popup & collect what the user did (probably just clicked OK)
-RESULT=$("$JAMFHELPER" -windowType utility -title "$TITLE" -description "$DESC" -button1 "OK")
+RESULT=$("$JAMFHELPER" -windowType hud -lockHUD -title "$TITLE" -heading "$HEADING" -description "$DESC" -button1 "Continue" -button2 "More Info" -defaultButton 1 -icon "$ICON")
 
 #If's to check the $RESULT variable to see what the user did, and take some action. 
-if [ "$RESULT" == 0 ]; then
+if [[ "$RESULT" == 0 ]]; then
     # do button1 stuff
     # echo "OK was pressed!"
     /usr/bin/open -b com.apple.systempreferences /System/Library/PreferencePanes/Accounts.prefPane
     exit 0
-
-#What do if cancel was pressed, enable if you want to put the cancel button back
-#elif [ "$RESULT" == 2 ]; then
+elif [[ "$RESULT" == 2 ]]; then
 #    # do button2 stuff
-#    echo "Cancel was pressed!"
-#    exit 1
-
+  /bin/launchctl asuser "$LOGGED_IN_UID" sudo -iu "$LOGGED_IN_USER" open https://google.com #replace url with some informative doc
 fi
+
+exit 0
